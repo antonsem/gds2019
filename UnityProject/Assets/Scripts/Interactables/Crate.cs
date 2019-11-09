52c938cf2e6c526fe79a4f949a859f4cc91f90c7
Anton <anton.semchenko@ar-vizor.cz>
2019-11-09 13:22:59 +0100

    UI rearranged

--- UnityProject/Assets/Scripts/Interactables/Crate.cs
+++ UnityProject/Assets/Scripts/Interactables/Crate.cs
@@ -6,6 +6,13 @@ public class Crate : MonoBehaviour, IInteratable
 {
     public void Interact()
     {
-        ExtraTools.PopUp.Instance.Register(string.Format("A very interesting {0}!", name), null, new ExtraTools.MessageButton("OK! :D", null), new ExtraTools.MessageButton("But is it though?", null));
+        ExtraTools.PopUp.Instance.Register(string.Format("A very interesting {0}!", name), null, 
+            new ExtraTools.MessageButton("OK! :D", null), 
+            new ExtraTools.MessageButton("But is it though?", Answer));
+    }
+
+    private void Answer()
+    {
+        ExtraTools.PopUp.Instance.Register("But of course it is!");
     }
 }
