2477952d00824dc5ef1911684e0a2ee13fb7fc76
YordDev <57544713+YordDev@users.noreply.github.com>
2019-11-09 20:08:57 +0100

    new robot and enemy

--- UnityProject/Assets/Scripts/Testing/Wellcome.cs
+++ UnityProject/Assets/Scripts/Testing/Wellcome.cs
@@ -7,6 +7,12 @@ public class Wellcome : MonoBehaviour, ITrigger
 {
     public void Trigger()
     {
+        StartCoroutine(message());
+    }
+
+    IEnumerator message()
+    {
+        yield return new WaitForSeconds(2);
         PopUp.Instance.Register("Zony: I couldn't believe my circruits first too, but its seems like the rumor is true. There is a lost puppy somewhere there in the forest, and if I find it i will be famous. The guy with the puppy.");
         Destroy(transform.gameObject);
     }
