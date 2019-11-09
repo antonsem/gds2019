0497339bf5d3072724fdc3d603b4fdd8b7f0f57f
Anton <anton.semchenko@ar-vizor.cz>
2019-11-09 12:24:15 +0100

    Player movement

---
+++ UnityProject/Assets/Scripts/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Constants
+{
+    public static class Tags
+    {
+
+    }
+
+    public static class Strings
+    {
+
+    }
+
+    public static class Layers
+    {
+        public static readonly int player = LayerMask.NameToLayer("Player");
+    }
+}
--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -1,25 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ExtraTools;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
     [SerializeField]
     private PlayerStats stats;
-    [MyBox.ReadOnly]
+    [MyBox.ReadOnly, SerializeField]
     private Rigidbody rigid;
+
+    private Vector3 moveDir = Vector3.zero;
+    private List<IInteratable> interactables = new List<IInteratable>();
+
+    private void GetInput()
+    {
+        moveDir.x = Input.GetAxisRaw("Horizontal");
+        moveDir.z = Input.GetAxisRaw("Vertical");
+    }
 
     private void Move(Vector3 direction)
     {
+        direction.Normalize();
         rigid.velocity = direction * stats.MoveSpeed;
     }
 
-#if UNITY_EDITOR
+    private void Rotate(Vector3 newRot, float delta)
+    {
+        transform.forward = Vector3.Lerp(transform.forward, newRot, delta);
+    }
 
+    private void Update()
+    {
+        GetInput();
+        Move(moveDir);
+        if (moveDir != Vector3.zero)
+            Rotate(moveDir, Time.deltaTime * stats.RotationSpeed);
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.transform.GetComponent(out IInteratable interactable))
+    //    {
+
+    //    }
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+#if UNITY_EDITOR
+    [MyBox.ButtonMethod]
     private void GetRigidbody()
     {
-
+        rigid = GetComponent<Rigidbody>();
     }
 
     private void Reset()
