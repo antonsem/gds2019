086ba8fc329907728a8398891684ef54f229406b
YordDev <57544713+YordDev@users.noreply.github.com>
2019-11-10 10:00:47 +0100

    sounds

--- UnityProject/Assets/Scripts/Testing/Baddy.cs
+++ UnityProject/Assets/Scripts/Testing/Baddy.cs
@@ -8,12 +8,22 @@ public class Baddy : MonoBehaviour, IInteratable
     [SerializeField]
     private Enemy enemy;
 
+    private AudioSource audio;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     public void Interact()
     {
-        PopUp.Instance.Register("Come at me bruh!", null, new MessageButton("Nah...", null), new MessageButton("You asked for it bruh!", DoFight));
+        audio.PlayOneShot(audio.clip);
+        StartCoroutine(InteractDelay(audio.clip.length));
     }
 
-   
+    IEnumerator InteractDelay(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        PopUp.Instance.Register("Come at me bruh!", null, new MessageButton("Nah...", null), new MessageButton("You asked for it bruh!", DoFight));
+    }
     private void DoFight()
     {
         CombatManager.enemy = enemy;
