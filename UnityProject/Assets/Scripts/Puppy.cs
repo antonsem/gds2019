ec6e532e52f6be91931ecbcc297eccd61b5e2fd0
Anton <anton.semchenko@ar-vizor.cz>
2019-11-10 18:18:54 +0100

    Puppy!!!

---
+++ UnityProject/Assets/Scripts/Puppy.cs
@@ -0,0 +1,33 @@
+﻿using ExtraTools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puppy : MonoBehaviour, IInteratable
+{
+    [SerializeField]
+    private PlayerStats stats;
+    [SerializeField]
+    private AudioSource audio;
+
+    public void Interact()
+    {
+        stats.CanMove = false;
+        if (audio != null)
+        {
+            audio.PlayOneShot(audio.clip);
+            StartCoroutine(InteractDelay(audio.clip.length));
+        }
+        else
+        {
+            StartCoroutine(InteractDelay(0));
+        }
+    }
+
+    IEnumerator InteractDelay(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        PopUp.Instance.Register("YOU DID IT! YOU MANAGED TO SAVE THE PUPPY!!! YOU ARE THE LEGEND!", null,
+            new MessageButton("YEY!", null));
+    }
+}
