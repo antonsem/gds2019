c1da0642a22e421659ecb555e14721375ec4f42b
Martin Vejvoda <martin@weareyord.com>
2019-11-10 14:53:24 +0100

    Fix + Menu scene
    FIxed a combat scene bug + added the first version of Menu.

--- UnityProject/Assets/Scripts/Combat/LoadEnemyVisual.cs
+++ UnityProject/Assets/Scripts/Combat/LoadEnemyVisual.cs
@@ -7,8 +7,7 @@ public class LoadEnemyVisual : MonoBehaviour
     private void Awake()
     {
         GameObject go = Instantiate(CombatManager.enemyVisual);
-        go.transform.position = new Vector3(0, 0, -3);
-        go.transform.Rotate(new Vector3(0, 180, 0));
+        go.transform.position = new Vector3(0, 0, 0);
         go.transform.parent = this.transform;
     }
 }
---
+++ UnityProject/Assets/Scripts/UI/CreditsUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CreditsUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button backButton;
+    [SerializeField]
+    private GameObject menu;
+
+    private void Awake()
+    {
+        backButton.onClick.AddListener(OnBack);
+    }
+
+    private void OnBack()
+    {
+        menu.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
